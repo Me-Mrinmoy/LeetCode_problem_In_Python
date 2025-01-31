36d6110fc8b0ecb68c94ae1ed8ef1d9a77ca74b6
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-01-31 11:30:58 +0530

    Create main.py

---
+++ HARD/main.py
@@ -0,0 +1,9 @@
+# given two numbers from the user and check which is longest--
+
+num1 = int (input ("Enter the number1: "))
+num2 = int (input ("Enter the number2: "))
+
+if (num1 > num2):
+  print ('num1')
+else:
+  print ('num2')

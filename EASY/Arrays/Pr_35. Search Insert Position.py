95edb78df94e6c54bbda1e1ff2b48b29af401408
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-05-17 18:48:43 +0530

    Create Pr_35. Search Insert Position.py

---
+++ EASY/Arrays/Pr_35. Search Insert Position.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left  # This will be the insertion point if target is not found
+
+# Example usage:
+nums = [1, 3, 5, 6]
+target = 5
+solution = Solution()
+result = solution.searchInsert(nums, target)
+print(result)  # Output: 2

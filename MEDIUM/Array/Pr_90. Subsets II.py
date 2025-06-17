077055a22033503c3d1ba9c4348a0ec63ca55f83
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-06-17 20:16:08 +0530

    Create Pr_90. Subsets II.py

---
+++ MEDIUM/Array/Pr_90. Subsets II.py
@@ -0,0 +1,29 @@
+class Solution:
+    def subsetsWithDup(self, nums):
+        
+        nums.sort()
+        result = []
+        subset = []
+        
+        def backtrack(start):
+            
+            result.append(subset[:])
+            
+            for i in range(start, len(nums)):
+                
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                
+                subset.append(nums[i])
+                
+                backtrack(i + 1)
+                
+                subset.pop()
+        
+        backtrack(0)
+        return result
+
+
+solution = Solution()
+print(solution.subsetsWithDup([1, 2, 2]))  
+print(solution.subsetsWithDup([0]))        

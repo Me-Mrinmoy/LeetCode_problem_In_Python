649a9f31b5312d94a3904f06bd9e1a96d02dbcba
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-07-27 20:18:14 +0530

    Create Pr_419. Battleships in a Board.py

---
+++ MEDIUM/Array/Pr_419. Battleships in a Board.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countBattleships(self, board):
+        if not board:
+            return 0
+
+        m, n = len(board), len(board[0])
+        count = 0
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'X':
+                    # Skip if part of a ship already counted (left or above)
+                    if i > 0 and board[i-1][j] == 'X':
+                        continue
+                    if j > 0 and board[i][j-1] == 'X':
+                        continue
+                    count += 1
+
+        return count

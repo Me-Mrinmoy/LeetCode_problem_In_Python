391b788fde3ddadb3ef17fe2aed215608e0c62f7
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-08-02 10:47:25 +0530

    Create Pr_495. Teemo Attacking.py

---
+++ EASY/Arrays/Pr_495. Teemo Attacking.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findPoisonedDuration(self, timeSeries, duration):
+        if not timeSeries:
+            return 0
+        
+        total = 0
+        for i in range(1, len(timeSeries)):
+            total += min(duration, timeSeries[i] - timeSeries[i - 1])
+        
+        total += duration  # Add poison duration from the last attack
+        return total

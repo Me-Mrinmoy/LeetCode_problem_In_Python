530a82bfa67656a5beef8a846984058afc20130e
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2025-08-19 09:31:29 +0530

    Create Pr_823. Binary Trees With Factors.py

---
+++ MEDIUM/Array/Pr_823. Binary Trees With Factors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numFactoredBinaryTrees(self, arr):
+        MOD = 10**9 + 7
+        arr.sort()
+        dp = {}
+        index = {x: i for i, x in enumerate(arr)}  # map value → index
+
+        for i, x in enumerate(arr):
+            dp[x] = 1  # single-node tree
+            for j in range(i):  # check factors smaller than x
+                a = arr[j]
+                if x % a == 0:
+                    b = x // a
+                    if b in dp:
+                        dp[x] += dp[a] * dp[b]
+            dp[x] %= MOD
+
+        return sum(dp.values()) % MOD
